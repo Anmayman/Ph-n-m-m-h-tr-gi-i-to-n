00e6679308bbffb79009ccfda9017f7aee86460d
Anmayman <93429080+Anmayman@users.noreply.github.com>
2021-11-04 10:51:31 +0700

    mot

--- version1.cpp
+++ version1.cpp
@@ -4,7 +4,7 @@ int main(){
     int a,b;
     cout<<"nhap vao hai so nguyen ";
     cin>>a>>b;
-    cout<<a<<"+"<<b<<"="<<a+b<<endl;
+    cout<<a<<"+"<<b<<"="<<a-b<<endl;
 
-    return 0;
+    return 0;git púh
 }
